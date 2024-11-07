2844ccc00df2e36c58925d8108d0426bab166cea
Suresh Kumar <jsureshkumarj1@gmail.com>
2024-11-07 13:20:31 +0530

    handling multiple \n chars delimited in the number string

--- tests/test.ts
+++ tests/test.ts
@@ -6,5 +6,7 @@ describe("String Parser", () => {
         expect(add("1,2")).toBe(3);
         expect(add("1,2,4")).toBe(7);
         expect(add("1\n2,4")).toBe(7);
+        expect(add("1\n2\n4,5")).toBe(12);
+        expect(add("1\n2\n4,5,6,7\n8")).toBe(33);
     });
 });
