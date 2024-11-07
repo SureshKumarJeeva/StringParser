9cdc1fcb6c8e0c0d10f28f25d9351c1e31a2c3f0
Suresh Kumar <jsureshkumarj1@gmail.com>
2024-11-07 13:02:52 +0530

    parsing string with \n as one of delimiter

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 function add(str:string):number{
     let sum:number = 0;
     let delimiter:string = ",";
-    str.split(delimiter).forEach(element => {
-        sum += Number(element);
+    str.split(delimiter).forEach(items => {
+        console.log(items);
+        let arrayItems:Array<string> = items.split("\n");
+        arrayItems.forEach(item =>{
+            console.log(item);
+            sum += Number(item);
+        });
     });
     return sum;
 }
--- tests/test.ts
+++ tests/test.ts
@@ -4,5 +4,7 @@ describe("String Parser", () => {
     it("should sum string of numbers delimited by an operator", ()=>{
         expect(add("")).toBe(0);
         expect(add("1,2")).toBe(3);
+        expect(add("1,2,4")).toBe(7);
+        expect(add("1\n2,4")).toBe(7);
     });
 });
