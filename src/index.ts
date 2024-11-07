0fadae38fe2a9a4df43e1e04dd97a9f14102b3ac
Suresh Kumar <jsureshkumarj1@gmail.com>
2024-11-07 13:43:08 +0530

    handle any special char as delimiter in the string

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
+/*
+* Function to find the sum of numbers in a string delimited by special char
+* @param str: string that contains number demilited by comma or special char
+* return : number showing the sum of all numbers in the string
+*/
 function add(str:string):number{
     let sum:number = 0;
     let delimiter:string = ",";
+    //condition to check if string contains delimiter char and extract it, if so
+    if(str.substring(0,2) == "//"){
+        delimiter = str.substring(2,str.indexOf("\n"));
+        str = str.substring(str.indexOf("\n") + 1);
+    }
+    //parse through numbers in provided string and find the sum
     str.split(delimiter).forEach(items => {
         console.log(items);
         let arrayItems:Array<string> = items.split("\n");
--- tests/test.ts
+++ tests/test.ts
@@ -1,12 +1,13 @@
 import  add  from '../src/index';
 
 describe("String Parser", () => {
-    it("should sum string of numbers delimited by an operator", ()=>{
+    it("should sum string of numbers delimited by a special char", ()=>{
         expect(add("")).toBe(0);
         expect(add("1,2")).toBe(3);
         expect(add("1,2,4")).toBe(7);
         expect(add("1\n2,4")).toBe(7);
         expect(add("1\n2\n4,5")).toBe(12);
         expect(add("1\n2\n4,5,6,7\n8")).toBe(33);
+        expect(add("//;\n1\n2\n4;5;6;7\n8")).toBe(33);
     });
 });
